b3fbc0b1018844b0293a6e2b15594f1f4d953d95
hiromiogawa <44115109+hiromiogawa@users.noreply.github.com>
2020-05-09 21:18:17 +0900

    aws s3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,14 +1,14 @@
 CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
-    provider:              'AWS',                        # required
-    aws_access_key_id:     ENV['AWS_S3_ACCESS_KEY_ID'],  # required
-    aws_secret_access_key: ENV['AWS_S3_SECRET_ACCESS_KEY'],     # required
-    region:                'us-east-1',                   # optional, defaults to 'us-east-1' オハイオ＝us-east-2
-    #host:                  's3.example.com',             # optional, defaults to nil
-    #endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
+    provider: 'AWS',
+    aws_access_key_id: ENV['ACCESS_KEY_ID'],
+    aws_secret_access_key: ENV['SECRET_ACCESS_KEY'],
+    region: 'us-east-1' # リージョンの位置によって記述が異なります
   }
-  config.fog_directory  = 'bwh-img'                # required
-  config.fog_public     = false                                                 # optional, defaults to true
-  config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" } # optional, defaults to {}
+
+    case Rails.env
+    when 'production'
+        config.fog_directory  = 'bwh-img'
+        config.asset_host = 'https://s3.amazonaws.com/bwh-img'
+    end
 end
